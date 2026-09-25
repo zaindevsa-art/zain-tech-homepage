6cc015d01222ca9c263866d88c32add673148862
zaindevsa-art <zaindevsa@gmail.com>
2026-09-25 21:58:15 +0300

    feat: assemble high quality hero image

---
+++ src/assets/hero-image-data.ts
@@ -0,0 +1,21 @@
+import p1 from "./hero-data/part01";
+import p2 from "./hero-data/part02";
+import p3 from "./hero-data/part03";
+import p4 from "./hero-data/part04";
+import p5 from "./hero-data/part05";
+import p6 from "./hero-data/part06";
+import p7 from "./hero-data/part07";
+import p8 from "./hero-data/part08";
+import p9 from "./hero-data/part09";
+
+export const heroBusinesswomanDataUrl =
+  "data:image/avif;base64," +
+  p1 +
+  p2 +
+  p3 +
+  p4 +
+  p5 +
+  p6 +
+  p7 +
+  p8 +
+  p9;
